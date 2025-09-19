8e9d6d5bc76736094e0c746ed72d1e12281b4dfa
alihoda4235 <alidoncoljob@gmail.com>
2025-09-20 01:13:42 +0530

    for learning boilerplate created

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,6 @@ import { ethers } from 'ethers';
 import logo from '../assets/logo.svg';
 
 const Navigation = ({ account, setAccount }) => {
-<<<<<<< HEAD
-    const connectHandler = async () => {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const account = ethers.utils.getAddress(accounts[0])
-        setAccount(account);
-    }
-=======
->>>>>>> aliswork
 
 }
 
